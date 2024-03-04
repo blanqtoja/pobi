9629f1385c7e5e1dde3c3aec6fb7b83c149a8f66
Aleksander Kaźmierczak <251544@edu.p.lodz.pl>
2024-03-04 22:52:27 +0100

    rm build

--- kazmierczak/introduction/build/library/CMakeFiles/MathLibrary.dir/src/math_helpers.cpp.o.d
+++
@@ -1,4 +0,0 @@
-library/CMakeFiles/MathLibrary.dir/src/math_helpers.cpp.o: \
- /mnt/c/Users/Olek/POBI/start/kazmierczak/introduction/library/src/math_helpers.cpp \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/Olek/POBI/start/kazmierczak/introduction/library/include/math_helpers.h
